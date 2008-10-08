b70716d797b4cf0a104c93c2575db492acb4bae0
jeffgrigg@charter.net <jeffgrigg@charter.net@ea3a757c-3454-0410-afdb-8faa49648a29>
2008-10-08 21:54:37 +0000

    Refactoring:  - Add JavaDoc.  - Change 'pendingPrefixes' field from List to Set.  - Rename 'elementData' to be 'elementMetadata', to properly match its type.  - Inline two local variables in 'end' method; used only once, to fetch ElementMetadata property values.  - Delete rudundant code in 'start(,)' method:  Namespace prefix was being added to 'pendingPrefixes' List/Set twice (when 'checkMe && hasPrefix' true).

--- java/src/com/ociweb/xml/ElementMetadata.java
+++ java/src/com/ociweb/xml/ElementMetadata.java
@@ -3,10 +3,28 @@ package com.ociweb.xml;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * Each instance of this class represents the information about one XML
+ * <code>Element</code>, its contents, and its context.
+ */
 /* package */ final class ElementMetadata {
 
+    /**
+     * XML <a href="http://www.w3.org/TR/2008/PER-xml-20080205/#NT-Name">
+     * <code>Name</code></a> of an XML <code>Element</code>, with optional XML
+     * Namespace "colon" prefix.
+     */
     private final String qualifiedName;
+
+    /**
+     * <code>true</code> if and only if this XML Element is the start/root/base
+     * of an XML Fragment that is a comment representing possible XML elements.
+     */
     private final boolean isInComment;
+
+    /**
+     * XML Namespace prefixes defined in this XML Element.
+     */
     private final Set<String> prefixes = new HashSet<String>();
 
     /* package */ ElementMetadata(final String qualifiedName, final boolean isInComment) {
